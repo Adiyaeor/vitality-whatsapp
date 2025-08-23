47d190d8cc975b48db38ea5a6347a8d12e4986a8
Adiyaeor <adiyaeor@gmail.com>
2025-08-23 14:51:00 +0300

    Delete page.jsx

--- page.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-export default function Page() {
-  return (
-    <div style={{padding:20, direction:"rtl"}}>
-      <h1>היי מאמן/ת</h1>
-      <p>כאן יוצגו המתאמנים שלך (היום / מחר / אתמול) ותיבות לשליחת הודעה בוואטסאפ.</p>
-    </div>
-  );
-}
